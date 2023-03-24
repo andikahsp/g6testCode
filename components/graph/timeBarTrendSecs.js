8d04ad9fbc568ce3b346fadd85b99a06234a2e8d
RayHoTY <Ray.Ho@custodiotech.com.sg>
2023-03-24 12:56:35 +0800

    added auto create + remove node combo/grouping

--- components/graph/timeBarTrendSecs.js
+++ components/graph/timeBarTrendSecs.js
@@ -44,12 +44,12 @@ function populateNodesEdges (jsonData){
    
    // node and edge definition will be based on backend logic                  
    const data = {
-     nodes:[
-       { id: 'node0', ...circle, label: jsonData.computerName, date: jsonData.logsourceTime  },
+      nodes:[
+       { id: 'node0', ...circle, label: jsonData.computerName, date: jsonData.logsourceTime },
        { id: 'node1', ...circle, label: jsonData.originatingComputer, date: jsonData.logsourceTime },
        { id: 'node2', ...triangle, label: jsonData.logonProcess, date: 1636095550 },    
        { id: 'node3', ...triangle, label: `vector`, date: 1636095554 },    
-      ],
+      ], 
      edges: [
        { source: 'node0', 
          target: 'node1', 
@@ -65,13 +65,16 @@ function populateNodesEdges (jsonData){
  }
 
 
+let nodeA = "";
+let nodeB = "";
+let comboNumber = 0;
+let selectedComboId = "";
+let callBackComboHas1NodeOrLess = "";
 
 const TimeBarTrendSecs = () => {
     const ref = React.useRef(null)
     const [graph, setGraph] = useState(null);
     const [timeBar, setTimeBar] = useState(null);
-    const [frequency, setFrequency] = useState(null);
-  
 
     useEffect(() => {
       const G6 = require('@antv/g6');
@@ -93,7 +96,7 @@ const TimeBarTrendSecs = () => {
       // time = 6:59.08 am
       // window: 6:58.59am  - 6:59.17 am (60seconds)
       for (let i = axisMin; i < axisMax; i++) {
-        console.log(`i = ${i}`);
+        //console.log(`i = ${i}`);
         timeBarData.push({
           date: i,
           value: Math.round(Math.random() * 200),
@@ -103,7 +106,7 @@ const TimeBarTrendSecs = () => {
 
 
       const nodeSize = 36;
-      console.log(G6.TimeBar);
+      //console.log(G6.TimeBar);
       const newTimebar = new G6.TimeBar({
         x: 0,
         y: 0,
@@ -114,8 +117,7 @@ const TimeBarTrendSecs = () => {
         tick: {
           tickLabelFormatter: (d) => {
             // convert the data accordingly
-            console.log(`d => ${JSON.stringify(d, null, 3)}`);
-            setFrequency(d.value)
+           // console.log(`d => ${JSON.stringify(d, null, 3)}`);
             return getUTCHrMinSec(d.date);
         },
           tickLabelStyle:{ 
@@ -233,8 +235,8 @@ const TimeBarTrendSecs = () => {
     
       
       // constrained the layout inside the area
-      // x: offset dist from left in px
-      // y: offset dist from top in px
+      // x: offset dist from left in px ? X-coordinate of the first node?
+      // y: offset dist from top in px ? Y-coordinate of the first node?
       const constrainBox = { x: 60, y: 50, width: 800, height: 500 };
       
       const onTick = () => {
@@ -242,7 +244,7 @@ const TimeBarTrendSecs = () => {
         let maxx = -99999999;
         let miny = 99999999;
         let maxy = -99999999;
-        let maxsize = -9999999;
+        let maxsize = -9999999; //<---- need to follow up
         nodeEdgeData.nodes.forEach((node) => {
           if (minx > node.x) {
             minx = node.x;
@@ -265,7 +267,7 @@ const TimeBarTrendSecs = () => {
         });
       };
       
-      console.log(G6.Graph);
+      //console.log(G6.Graph);
       const newGraph = new G6.Graph({
         container: ref.current,
         width: width,
@@ -288,11 +290,11 @@ const TimeBarTrendSecs = () => {
           onTick,
           
         },
-
         defaultNode: {
           size: nodeSize,
           type: 'circle',
           style: {
+            position: 'top',
             stroke: '#4fdcff',
             fill: 'transparent',
             lineWidth: 4,
@@ -307,8 +309,36 @@ const TimeBarTrendSecs = () => {
             }
           },
         },
+        // Set groupByTypes to false to get rendering result with reasonable visual zIndex for combos
+        groupByTypes: false,
+        defaultCombo: {
+          type: 'circle',
+          size: [16], // The minimum size of the Combo
+          padding: [16, 10, 3, 10],
+          style: {
+            position:'bottom',
+            stroke: 'gray',
+            fill: 'transparent',
+            lineWidth: 1,
+          },
+          labelCfg: {
+            refY: 15,
+            position: 'bottom',
+            style: {
+              fill: '#000000A6',
+              fontFamily: 'Arial',
+              fontSize: 11
+            }
+          },
+        },
+        comboStateStyles: {
+          dragenter: {
+            lineWidth: 4,
+            stroke: '#FE9797',
+          },
+        },
         defaultEdge: {
-          type: 'quadratic',
+          type: 'line',
           style: {
             stroke: '#5f6266',
             lineWidth: 2.2,
@@ -340,6 +370,8 @@ const TimeBarTrendSecs = () => {
               type: 'zoom-canvas',
               enableOptimize: false, // enable the optimize to hide the shapes beside nodes' keyShape
             },
+            'drag-combo', 
+            'collapse-expand-combo'
           ],
         },
         labelCfg: {
@@ -366,16 +398,45 @@ const TimeBarTrendSecs = () => {
       });
       newGraph.data(nodeEdgeData);
       newGraph.render();
-      newGraph.on('node:mouseenter', evt => {
-        console.log(`evt on mouse enter node = ${evt}`);
+
+      newGraph.on('node:mouseenter', (evt, e) => {
         newGraph.setItemState(evt.item, 'hover', true)
       })
-  
+      
+      newGraph.on('node:mouseenter', (e) => {
+        console.log('node:mouseenter e =', e);
+        console.log(`mouseenter node ID = ${e.item._cfg.id}`)
+          nodeA = e.item._cfg.id;
+        //console.log(`mouseentered\n x: ${e.x}, y: ${e.y}`)
+      })
+      
       newGraph.on('node:mouseleave', evt => {
-        console.log(`evt on mouse leave node = ${evt}`);
         newGraph.setItemState(evt.item, 'hover', false)
       })
-  
+
+      // check that the node that is being dragged, does not have a  comboId,  
+      newGraph.on('node:dragenter', (e) => {
+        console.log(`dragEnter node ID = ${e.item._cfg.id}`);
+        if ( !('comboId' in e.item._cfg.model) || e.item._cfg.model.comboId === undefined ) { // if it has a comboId, do not create combo
+          if(nodeA !== "" && e.item._cfg.id !== nodeA ){ 
+            nodeB = e.item._cfg.id;
+            newGraph.createCombo({
+              id: `combo${comboNumber}`,   
+            }, [`${nodeA}`, `${nodeB}`]);
+            comboNumber += 1; // <=== this is causing uncontrolled propagation.
+          }
+        }
+      });
+
+      newGraph.on('node:dragend', (e) => {
+        newGraph.getCombos().forEach((combo) => {
+          newGraph.setItemState(combo, 'dragenter', false);
+        });
+        console.log(`node dragend ID = ${e.item._cfg.id}`);
+        //console.log(`x: ${e.x}, y: ${e.y}`)
+      });
+
+
       newGraph.on('edge:mouseenter', evt => {
         newGraph.setItemState(evt.item, 'hover', true)
       })
@@ -383,6 +444,64 @@ const TimeBarTrendSecs = () => {
       newGraph.on('edge:mouseleave', evt => {
         newGraph.setItemState(evt.item, 'hover', false)
       })
+
+      newGraph.on('combo:mouseenter', (evt) => {
+        const { item } = evt;
+        selectedComboId = item._cfg.id;
+        console.log(`selectedComboId = ${selectedComboId}`);
+        callBackComboHas1NodeOrLess = comboHas1NodeOrLess(evt.currentTarget.cfg.nodes, selectedComboId);
+        newGraph.setItemState(item, 'active', true);
+      });
+      
+      newGraph.on('combo:mouseleave', (evt) => {
+        const { item } = evt;
+        newGraph.setItemState(item, 'active', false);
+      });
+      
+      newGraph.on('combo:dragenter', (e) => {
+        newGraph.setItemState(e.item, 'dragenter', true);
+      });
+      
+      
+      newGraph.on('combo:dragleave', (e) => {
+        console.log(`dragleave combo id = ${e.item._cfg.id}`)
+        newGraph.setItemState(e.item, 'dragenter', false);
+      });
+
+      newGraph.on(`combo:mousedown`, (e) => {
+        console.log(`...MOUSE UP`);
+        // there is only 1 node left in the array, then we can uncombo
+        if(callBackComboHas1NodeOrLess === true) {
+          console.log(`selectedComboId on node:mouseup = ${selectedComboId}`);
+          newGraph.uncombo(`${selectedComboId}`);
+          selectedComboId = "";
+        } 
+      });
+      // this function fails whenever there are no combo assignments
+      // we  allow uncombo, if selected combo has only 1 node
+      const comboHas1NodeOrLess = (nodesArray, selectedComboId) => {
+        let counter = 0;
+        for(let i = 0; i < nodesArray.length; i += 1){
+          if('comboId' in nodesArray[i]._cfg.model){
+            if(nodesArray[i]._cfg.model.comboId === selectedComboId && nodesArray[i]._cfg.model.comboId !== undefined){
+              counter += 1
+            }
+          }
+        }
+        console.log(`${selectedComboId} counter = ${counter}`);
+        if(counter == 0){
+          return true
+        }
+        return false
+      }
+
+  
+
+/*       newGraph.on('combo:dblclick', (e) => {
+        console.log(`combo double click e = ${e}`);
+        
+      }); */
+      
       
       /* if (typeof window !== 'undefined')
         window.onresize = () => {
@@ -393,6 +512,7 @@ const TimeBarTrendSecs = () => {
       
       setGraph(newGraph);
       setTimeBar(newTimebar);
+      //console.log(newGraph);
     }, [])
   
     return <div ref={ref}></div>
