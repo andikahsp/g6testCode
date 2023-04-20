89ddbc30ae0f48c2085c843ce38d7368dd407aab
RayHoTY <Ray.Ho@custodiotech.com.sg>
2023-04-20 17:25:09 +0800

    [BUG-FIX] [ VEdge Error] unsuccessful: get webpack not to bundle node modules

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
+const nodeExternals = require('webpack-node-externals');
 
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true, 
+  // webpack: (config) => {
+  //   config.cache.buildDependencies.mydeps = ['/nonexistent/file/location'];
+  //   return config;
+  // }
+  // webpack: (
+  //   config,
+  //   { snapshot }
+  // ) => {
+  //   config.snapshot.managedPaths = [/^(.+?[\\/]node_modules)[\\/]((?!@antv)).*[\\/]*/];
+  //   return config
+  // },
+  webpack: (config, {target, externalsPresets, externals }) =>  {
+    config.target = 'node';
+    config.externalsPresets = {node: true};
+    config.externals = [nodeExternals()];
+    return config
+  }
+}
