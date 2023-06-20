f03e4d420cde1a1e4554d0168513bd1513298691
RayHoTY <Ray.Ho@custodiotech.com.sg>
2023-06-20 11:03:26 +0800

    [CYGV3-Promax-integration-test01] populateNodesEdges Fn amended for CYG data output, source file switch
    - populateNodesEdges Fn amended for CYG data output,
    - source file for graph data switch
    - CYG node legend key added to imageURLs component for demo

---
+++ components/graph/parts/graphDataConfigOrg.js
@@ -0,0 +1,212 @@
+import { imageURLs } from "./imageURLs";
+
+export function populateNodesEdges (jsonData) {
+
+    // This format if we want to customise shapes and sizes according
+    // to data
+    
+    const image = { type: 'image', 
+                     size: 36};
+    const circle = { type: 'circle', 
+                     size: 48 };
+    
+    const triangle = { type: 'triangle',
+                       size: 20, 
+                       direction: 'up'};
+     
+     // node and edge definition will be based on backend logic                  
+     const data = {
+        nodes:[
+        //   { id: 'node0', ...circle, /* img: imageURLs[0], */ comboId: 'combo1',  label: /* 'node0' */jsonData.computerName, /* date: jsonData.logsourceTime */ inRange: true },
+        //   { id: 'node1', ...image, img: imageURLs[1], comboId: 'combo4',  label: 'node1'/* jsonData.logonProcess */, /* date: jsonData.logsourceTime */ inRange: true},
+        //  { id: 'node2', ...triangle, /* img: imageURLs[2], */ /* comboId: 'combo3',  */ label: /* 'node2' */jsonData.originatingComputer, /* date: 1636095550 */ inRange: true},    
+        { id: 'node3', ...image, img: imageURLs[3], comboId: 'combo5',  label: 'node3'/* `vector` */, /* date: 1636095551 */ inRange: true},  
+        { id: 'node4', ...image, img: imageURLs[4], comboId: 'combo6',  label: 'node4'/* `vector` */, /* date: 1636095552 */ inRange: true},  
+        //   { id: 'node5', ...image, img: imageURLs[5], /* comboId: 'combo6',   */label: 'node5'/* `vector` */, /* date: 1636095553 */ inRange: true}, 
+        //   { id: 'node6', ...image, img: imageURLs[6], comboId: 'combo4',  label: 'node6'/* `vector` */, /* date: 1636095553 */ inRange: true}, 
+        //   { id: 'node7', ...image, img: imageURLs[7], comboId: 'combo4', label: 'node7'/* `vector` */, /* date: 1636095553 */ inRange: true}, 
+        //   { id: 'node8', ...image, img: imageURLs[8], comboId: 'combo4', label: 'node8'/* `vector` */, /* date: 1636095553 */ inRange: true}, 
+        //   { id: 'node9', ...image, img: imageURLs[9], label: 'node9'/* `vector` */, /* date: 1636095552 */ inRange: true},  
+        //   { id: 'node10', ...image, img: imageURLs[10], label: 'node10'/* `vector` */, /* date: 1636095553 */ inRange: true}, 
+        //  { id: 'node11', ...image, img: imageURLs[11], label: 'node11'/* `vector` */, /* date: 1636095553 */ inRange: true}, 
+        //  { id: 'node12', ...image, img: imageURLs[12], label: 'node12'/* `vector` */, /* date: 1636095553 */ inRange: true}, 
+        //  { id: 'node13', ...image, img: imageURLs[13], label: 'node13'/* `vector` */, /* date: 1636095553 */ inRange: true},
+        //  { id: 'node14', ...image, img: imageURLs[14], label: 'pointer'/* `vector` */, /* date: 1636095553 */ inRange: true}, 
+        ], 
+       edges: [
+        // { 
+        //   id: 'edge1',
+        //   source: 'node0', 
+        //   target: 'node1', 
+        //   ttp: true,
+        //   frequency: '3', 
+        //   event: 'edge1',//`${jsonData.message}`,
+        //   inRange: true, 
+        //   date: 
+        //   [
+        //     1636095542,
+        //   ],
+        // },
+        { 
+          id: 'edge2',
+          source: 'node3',
+          target: 'node4', 
+          event: 'edge2',//`some other Event`
+          ttp: true, 
+          frequency: 6, 
+          inRange: true, 
+          date: 
+          [
+            1636095547,
+            1636095548,
+            1636095549,
+            1636095550,
+            1636095551,
+            1636095552
+          ],
+        },
+        // { 
+        //   id: 'edge3',
+        //   source: 'node4', 
+        //   target: 'node5', 
+        //   frequency: 3,
+        //   ttp: true, 
+        //   event: 'edge3',
+        //   inRange: true, 
+        //   date: 
+        //   [
+        //     1636095548,
+        //     1636095550,
+        //     1636095552
+        //   ],
+        // },
+        // { 
+        //   id: 'edge4',
+        //   source: 'node5', 
+        //   target: 'node9', 
+        //   frequency: 1,
+        //   ttp: true, 
+        //   event: 'edge4',
+        //   inRange: true,
+        //   date: 
+        //   [ 
+        //     1636095554,
+        //   ]
+        // },
+        // { 
+        //   id: 'edge5',
+        //   source: 'node9', 
+        //   target: 'node10', 
+        //   frequency: 1,
+        //   ttp: true, 
+        //   event: 'edge5',
+        //   inRange: true, 
+        //   date: 
+        //   [
+        //     1636095555,
+        //   ]
+        // },
+        // { 
+        //   id: 'edge6',
+        //   source: 'node0', 
+        //   target: 'node6', 
+        //   frequency: '1',
+        //   ttp: true, 
+        //   event: 'edge6',
+        //   inRange: true, 
+        //   date: 
+        //   [
+        //     1636095546,
+        //   ],
+        // },
+        // { 
+        //   id: 'edge7',
+        //   source: 'node0', 
+        //   target: 'node7', 
+        //   frequency: '21',
+        //   ttp: true, 
+        //   event: 'edge7',
+        //   inRange: true, 
+        //   date: 
+        //   [
+        //     1636095548,
+        //   ],
+        // },
+        // { 
+        //   id: 'edge8',
+        //   source: 'node0', 
+        //   target: 'node8', 
+        //   frequency: '21',
+        //   ttp: true, 
+        //   event: 'edge8',
+        //   inRange: true, 
+        //   date: 
+        //   [
+        //     1636095553,
+        //   ],
+        // },
+        { 
+          id: 'edge9',
+          source: 'node4', 
+          target: 'node3', 
+          frequency: 2,
+          ttp: false, 
+          event: 'edge9',
+          inRange: true, 
+          date: 
+          [
+            1636095546,
+            1636095547,
+          ]
+        },
+       ],
+       combos: [
+        // {
+        //   id: 'combo1',
+        //   label: 1,
+        //   parentId: 'combo2',
+        //   inRange: true, 
+        // },
+        // {
+        //   id: 'combo2',
+        //   label: 1,
+        //   parentId: 'combo3',
+        //   inRange: true, 
+        // },
+        // {
+        //   id: 'combo3',
+        //   label: 1,
+        //   inRange: true, 
+        // //   // parentId: '8'
+        // },
+        // {
+        //   id: 'combo4',
+        //   label: 4,
+        //   inRange: true, 
+        // },
+        {
+          id: 'combo5',
+             label: 1,
+          inRange: true, 
+          parentId: 'combo7'
+        },
+        {
+          id: 'combo6',
+          label: 1,
+          inRange: true,
+          parentId: 'combo8'
+        },
+        {
+          id: 'combo7',
+          label: 1,
+          inRange: true, 
+        },
+        {
+          id: 'combo8',
+          label: 1,
+          inRange: true, 
+        },
+       ]
+     } 
+     return data;
+   }
--- components/graph/parts/imageURLs.js
+++ components/graph/parts/imageURLs.js
@@ -1,5 +1,5 @@
 export const imageURLs = [
-    `https://cdn.pixabay.com/photo/2013/07/13/11/47/computer-158675_960_720.png`,
+    `https://cdn.pixabay.com/photo/2013/07/13/11/47/computer-158675_960_720.png`, 
     `https://cdn.pixabay.com/photo/2017/06/03/07/51/mandala-2368148_1280.png`,
     `https://cdn.pixabay.com/photo/2013/07/13/12/33/computer-159837_960_720.png`,
     `https://cdn.pixabay.com/photo/2022/02/20/17/30/network-7024978_960_720.png`,
@@ -13,5 +13,15 @@ export const imageURLs = [
     `https://cdn.pixabay.com/photo/2017/09/07/10/25/logo-2724481_960_720.png`,
     `https://cdn.pixabay.com/photo/2015/11/09/19/15/spiral-1035714_960_720.png`,
     `https://cdn.pixabay.com/photo/2017/07/22/09/35/spiro-2528208_960_720.png`,
-    `https://cdn.pixabay.com/photo/2023/03/22/02/02/skier-7868502_960_720.png`
+    `https://cdn.pixabay.com/photo/2023/03/22/02/02/skier-7868502_960_720.png`, // i = 14
+    `https://cdn.pixabay.com/photo/2014/03/24/13/49/avatar-294480_960_720.png`, // i = 15, User
+    `https://cdn.pixabay.com/photo/2021/07/25/08/07/advisors-6491207_1280.png`, // i = 16, Group
+    `https://cdn.pixabay.com/photo/2013/07/13/11/47/computer-158675_1280.png`, // i = 17, Computer
+    `https://cdn.pixabay.com/photo/2012/04/01/12/46/folder-23275_960_720.png`, // i = 18, Registry
+    `https://cdn.pixabay.com/photo/2014/04/03/00/40/document-309065_960_720.png`, // i = 19, File
+    `https://cdn.pixabay.com/photo/2013/07/12/18/54/action-153973_960_720.png`, // i = 20, Process
+    `https://cdn.pixabay.com/photo/2012/04/02/16/17/printer-24860_960_720.png`, // i = 21, Port
+    `https://cdn.pixabay.com/photo/2012/04/13/11/06/shield-31869_960_720.png`, // i = 22, Firewall
+    `https://cdn.pixabay.com/photo/2012/04/28/19/00/mail-44042_960_720.png`, // i = 23, Email
+    `https://cdn.pixabay.com/photo/2012/04/13/00/17/question-mark-31190_960_720.png` // i = 24, question mark
 ]
