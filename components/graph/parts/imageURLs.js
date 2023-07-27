30323bf25bbeb423b942c1880a1b3841a4d8ae62
RayHoTY <Ray.Ho@custodiotech.com.sg>
2023-07-27 22:20:19 +0800

    added IP address icon and additional comments for easier devmt

--- components/graph/parts/imageURLs.js
+++ components/graph/parts/imageURLs.js
@@ -1,18 +1,18 @@
 export const imageURLs = [
-    `https://cdn.pixabay.com/photo/2013/07/13/11/47/computer-158675_960_720.png`, 
-    `https://cdn.pixabay.com/photo/2017/06/03/07/51/mandala-2368148_1280.png`,
-    `https://cdn.pixabay.com/photo/2013/07/13/12/33/computer-159837_960_720.png`,
-    `https://cdn.pixabay.com/photo/2022/02/20/17/30/network-7024978_960_720.png`,
-    `https://cdn.pixabay.com/photo/2019/10/04/13/52/watercolor-4525736_960_720.png`,
-    `https://cdn.pixabay.com/photo/2014/04/02/10/45/spiral-304435_960_720.png`,
-    `https://cdn.pixabay.com/photo/2016/06/26/18/24/saw-1480971_960_720.png`,
-    `https://cdn.pixabay.com/photo/2022/11/23/18/31/birds-7612651_960_720.png`,
-    `https://cdn.pixabay.com/photo/2022/10/06/22/22/pizza-7503664_960_720.png`,
-    `https://cdn.pixabay.com/photo/2013/07/12/13/22/gear-146921_960_720.png`,
-    `https://cdn.pixabay.com/photo/2013/07/13/01/15/preferences-155386_960_720.png`,
-    `https://cdn.pixabay.com/photo/2017/09/07/10/25/logo-2724481_960_720.png`,
-    `https://cdn.pixabay.com/photo/2015/11/09/19/15/spiral-1035714_960_720.png`,
-    `https://cdn.pixabay.com/photo/2017/07/22/09/35/spiro-2528208_960_720.png`,
+    `https://cdn.pixabay.com/photo/2013/07/13/11/47/computer-158675_960_720.png`, // i = 0
+    `https://cdn.pixabay.com/photo/2017/06/03/07/51/mandala-2368148_1280.png`, // i = 1
+    `https://cdn.pixabay.com/photo/2013/07/13/12/33/computer-159837_960_720.png`, // i = 2
+    `https://cdn.pixabay.com/photo/2022/02/20/17/30/network-7024978_960_720.png`, // i = 3
+    `https://cdn.pixabay.com/photo/2019/10/04/13/52/watercolor-4525736_960_720.png`, // i = 4
+    `https://cdn.pixabay.com/photo/2014/04/02/10/45/spiral-304435_960_720.png`, // i = 5
+    `https://cdn.pixabay.com/photo/2016/06/26/18/24/saw-1480971_960_720.png`, // i = 6
+    `https://cdn.pixabay.com/photo/2022/11/23/18/31/birds-7612651_960_720.png`, //  i = 7
+    `https://cdn.pixabay.com/photo/2022/10/06/22/22/pizza-7503664_960_720.png`, // i = 8
+    `https://cdn.pixabay.com/photo/2013/07/12/13/22/gear-146921_960_720.png`, // i = 9
+    `https://cdn.pixabay.com/photo/2013/07/13/01/15/preferences-155386_960_720.png`, // i = 10
+    `https://cdn.pixabay.com/photo/2017/09/07/10/25/logo-2724481_960_720.png`, // i = 11
+    `https://cdn.pixabay.com/photo/2015/11/09/19/15/spiral-1035714_960_720.png`, // i = 12
+    `https://cdn.pixabay.com/photo/2017/07/22/09/35/spiro-2528208_960_720.png`, // i = 13
     `https://cdn.pixabay.com/photo/2023/03/22/02/02/skier-7868502_960_720.png`, // i = 14
     `https://cdn.pixabay.com/photo/2016/08/31/11/54/icon-1633249_960_720.png`, // i = 15, User
     `https://cdn.pixabay.com/photo/2021/07/25/08/07/advisors-6491207_1280.png`, // i = 16, Group
@@ -23,5 +23,6 @@ export const imageURLs = [
     `https://cdn.pixabay.com/photo/2012/04/02/16/17/printer-24860_960_720.png`, // i = 21, Port
     `https://cdn.pixabay.com/photo/2012/04/13/11/06/shield-31869_960_720.png`, // i = 22, Firewall
     `https://cdn.pixabay.com/photo/2012/04/28/19/00/mail-44042_960_720.png`, // i = 23, Email
-    `https://cdn.pixabay.com/photo/2012/04/13/00/17/question-mark-31190_960_720.png` // i = 24, question mark
+    `https://cdn.pixabay.com/photo/2021/07/25/08/07/address-6491205_960_720.png`, // i = 24, IP address
+    `https://cdn.pixabay.com/photo/2012/04/13/00/17/question-mark-31190_960_720.png` // i = 25, question mark
 ]
