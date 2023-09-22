c01bc07079c57fdfd8b5ded1434daf4d4d44e568
RayHoTY <Ray.Ho@custodiotech.com.sg>
2023-09-22 23:20:16 +0800

    promax blank template for demo work

---
+++ components/graph/PromaxG6Template.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+// import { data as jsonData } from './sourceActiveDirLog';
+// import { data as jsonData } from './sourceCheckPointLog';
+// import { data as jsonData } from './sourceWinLog';
+import { data as jsonData } from './sourceWinLog2';
+//import { data as jsonData } from './sourceIocTtp';
+import { cCircleComboShape, circleNodeShape, fundPolyline, customQuadratic,} from "./parts/elements";
+import { getUTCHrMinSec } from "./utilities/convertUTC";
+import { populateNodesEdges } from "./parts/graphDataConfig";
+
+let log = console.log; 
+let nodeA = "";
+let nodeB = "";
+let nodeDrag = false;
+let comboDrop = false;
+let dragCombo;
+let dragleaveCombo;
+let draggedOverCombos = [];
+let comboDraggedOver; 
+
+const templateG5 = () => {
+    const ref = React.useRef(null)
+    const [graph, setGraph] = useState(null);
+
+
+    useEffect(() => {
+      const G6 = require('@antv/g6');
+
+
+      setGraph(graph);
+      }, [])
+    
+      return <div ref={ref}></div>
+    }
+    
+
+       
+  export default templateG5
+
+
+  
+
